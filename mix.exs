77fd6b54d69dbba9d57fba6d9235adda7b747be1
gonzalinux <gonzaleonrey@hotmail.com>
2026-03-02 19:38:42 +0100

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule ElixirGateway.MixProject do
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_), do: ["lib"]
+  defp elixirc_paths(_), do: ["lib", "priv/static_epmd"]
 
   # Specifies your project dependencies.
   #
