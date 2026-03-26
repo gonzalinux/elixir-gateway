6630ddcbee97444922d7e9914902e8c5ee4ddb9c
gonzalinux <gonzaleonrey@hotmail.com>
2026-03-26 10:11:17 +0100

    Fix

--- lib/elixir_gateway/cluster/load_distributor.ex
+++ lib/elixir_gateway/cluster/load_distributor.ex
@@ -444,9 +444,6 @@ defmodule ElixirGateway.Cluster.LoadDistributor do
       key == host ->
         true
 
-      key in ["default_any", "default"] ->
-        true
-
       String.starts_with?(key, "*") ->
         suffix = String.slice(key, 1..-1//1)
         String.ends_with?(host, suffix)
